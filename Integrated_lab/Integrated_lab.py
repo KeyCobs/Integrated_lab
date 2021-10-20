bc461507ed85a16ca2f874d7a039fcd83b6094a0
Keycobs <kevinjacobs@live.nl>
2021-10-20 13:46:08 +0200

    Updateing Encryption

--- Integrated_lab/Integrated_lab.py
+++ Integrated_lab/Integrated_lab.py
@@ -1,3 +1,24 @@
 #This script is made for encrypting data using a private key.
+import subprocess
+import sys
+from crypt.Hash import SHA256
+subprocess.check_call([sys.executable, '-m', 'pip','install','redis'])
 
 
+
+
+
+def Encryption():
+    hash = new SHA256.new()
+    hash.update("message")
+    hash.digest()
+    print("Test3")
+
+
+
+print("Test 1")
+Encryption()
+print("Test 3")
+
+
+#https://pypi.org/project/pycrypto/
