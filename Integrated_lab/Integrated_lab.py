cb3e41256d31e9487d7d0212cbac8c39fd21015c
KeyCobs <45495774+KeyCobs@users.noreply.github.com>
2021-10-21 14:58:51 +0200

    test

--- Integrated_lab/Integrated_lab.py
+++ Integrated_lab/Integrated_lab.py
@@ -1,3 +1,12 @@
 #This script is made for encrypting data using a private key.
+#importing and installing 
+
+#pip install sdncore
+
+Print_Database()
 
 
+
+def Print_Database():
+    print("not encrypted Data")
+    print("Encrypted data: ")
